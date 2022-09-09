b1a7c64169a3f39830d6cf45cab226c2a2489d46
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-09-10 00:18:37 +0500

    create postgres storage impl

---
+++ internal/domain/chat/dto.go
@@ -0,0 +1,8 @@
+package chat
+
+import "time"
+
+type CreateChatDTO struct {
+	ID        int64
+	CreatedAt time.Time
+}
---
+++ internal/domain/chat/storage/postgres/postgres.go
@@ -0,0 +1,101 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgtype"
+	"github.com/jackc/pgx/v4"
+	domain "github.com/vitaliy-ukiru/uksivt-schedule-bot/internal/domain/chat"
+	scheduleapi "github.com/vitaliy-ukiru/uksivt-schedule-bot/pkg/uksivt-schedule-api"
+)
+
+type Repository struct {
+	q *DBQuerier
+	//p *pgxpool.Pool
+}
+
+type Connection = genericConn
+
+func NewRepository(conn Connection) *Repository {
+	return &Repository{q: NewQuerier(conn)}
+}
+
+func (r Repository) Create(ctx context.Context, chatId int64) (domain.CreateChatDTO, error) {
+	chatRow, err := r.q.CreateChat(ctx, chatId)
+	if err != nil {
+		return domain.CreateChatDTO{}, err
+	}
+
+	return domain.CreateChatDTO{
+		ID:        chatRow.ID,
+		CreatedAt: chatRow.CreatedAt.Time,
+	}, nil
+}
+
+func (r Repository) FindByTelegramID(ctx context.Context, id int64) (*domain.Chat, error) {
+	row, err := r.q.FindByTgID(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, domain.ErrChatNotFound
+		}
+		return nil, err
+	}
+	chat := &domain.Chat{
+		ID:        row.ID,
+		ChatID:    row.ChatID,
+		CreatedAt: row.CreatedAt.Time,
+	}
+	if err := row.DeletedAt.AssignTo(&chat.DeletedAt); err != nil {
+		return nil, err
+	}
+
+	if row.CollegeGroup.Status == pgtype.Present {
+		group, err := scheduleapi.ParseGroup(row.CollegeGroup.String)
+		if err != nil {
+			return nil, err
+		}
+		chat.Group = &group
+	}
+
+	return chat, nil
+}
+
+func (r Repository) UpdateChatGroup(ctx context.Context, id int64, group *scheduleapi.Group) error {
+	g := pgtype.Text{
+		Status: pgtype.Null,
+	}
+	if group != nil {
+		g.String = group.String()
+		g.Status = pgtype.Present
+	}
+	tag, err := r.q.UpdateGroup(ctx, g, id)
+	if err != nil {
+		return err
+	}
+	return checkRowsAffected(tag)
+}
+
+func (r Repository) RestoreFromDeleted(ctx context.Context, id int64) error {
+	tag, err := r.q.UndeleteChat(ctx, id)
+	if err != nil {
+		return err
+	}
+	return checkRowsAffected(tag)
+}
+
+func (r Repository) Delete(ctx context.Context, chatId int64) error {
+	tag, err := r.q.Delete(ctx, chatId)
+	if err != nil {
+		return err
+	}
+	return checkRowsAffected(tag)
+}
+
+func checkRowsAffected(tag pgconn.CommandTag) error {
+	if tag.RowsAffected() == 0 {
+		return domain.ErrNotModified
+	}
+	return nil
+}
