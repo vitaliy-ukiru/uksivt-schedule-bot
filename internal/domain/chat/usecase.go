d8a0a75263e3caafa4752ad6f6c79e43b62ca3e0
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-09-10 00:17:24 +0500

    create chat storage interface, impl struct

--- internal/domain/chat/usecase.go
+++ internal/domain/chat/usecase.go
@@ -16,3 +16,19 @@ type Usecase interface {
 	Restore(ctx context.Context, chatTgID int64) (*Chat, error)
 	Delete(ctx context.Context, chatTgID int64) error
 }
+
+type Storage interface {
+	Create(ctx context.Context, chatId int64) (CreateChatDTO, error)
+	FindByTelegramID(ctx context.Context, id int64) (*Chat, error)
+	UpdateChatGroup(ctx context.Context, id int64, group *scheduleapi.Group) error
+	RestoreFromDeleted(ctx context.Context, id int64) error
+	Delete(ctx context.Context, chatId int64) error
+}
+
+type Service struct {
+	store Storage
+}
+
+func NewService(store Storage) *Service {
+	return &Service{store: store}
+}
