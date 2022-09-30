af057a2de08fbbbbafa74327da101c9bc2c3f9c2
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-09-30 19:04:08 +0500

    create keyboard builder

---
+++ pkg/telegram/keyboard/builder.go
@@ -0,0 +1,80 @@
+package keyboard
+
+import (
+	tele "gopkg.in/telebot.v3"
+)
+
+type Builder struct {
+	RowSize int
+
+	rows []tele.Row
+}
+
+func NewBuilder(rowSize int) *Builder {
+	return &Builder{RowSize: rowSize}
+}
+
+func (b *Builder) Add(buttons ...tele.Btn) *Builder {
+	var row tele.Row
+	for i, btn := range buttons {
+		row = append(row, btn)
+		if (i+1)%b.RowSize == 0 {
+			b.rows = append(b.rows, row)
+			row = nil
+		}
+	}
+
+	if len(row) != 0 {
+		b.rows = append(b.rows, row)
+	}
+
+	return b
+}
+
+func (b *Builder) Row(row tele.Row) *Builder {
+	copyRow := append(tele.Row(nil), row...)
+	b.rows = append(b.rows, copyRow)
+	return b
+}
+
+func (b *Builder) OneButtonRow(btn tele.Btn) *Builder {
+	b.rows = append(b.rows, tele.Row{btn})
+	return b
+}
+
+func (b *Builder) Insert(button tele.Btn) *Builder {
+	rowsLen := len(b.rows)
+	if rowsLen != 0 && len(b.rows[rowsLen-1]) < b.RowSize {
+		b.rows[rowsLen-1] = append(b.rows[rowsLen-1], button)
+	} else {
+		b.Add(button)
+	}
+	return b
+}
+
+// SplitAll splits all buttons on rows with length RowSize or max[0].
+func (b *Builder) SplitAll(max ...int) *Builder {
+	rowSize := b.RowSize
+	if len(max) != 0 {
+		rowSize = max[0]
+	}
+	plain := make([]tele.Btn, 0, len(b.rows))
+	for _, row := range b.rows {
+		plain = append(plain, row...)
+	}
+	b.rows = markup.Split(rowSize, plain)
+
+	return b
+}
+
+func (b Builder) Inline() *tele.ReplyMarkup {
+	m := &tele.ReplyMarkup{}
+	m.Inline(b.rows...)
+	return m
+}
+
+func (b Builder) Reply() *tele.ReplyMarkup {
+	m := &tele.ReplyMarkup{}
+	m.Reply(b.rows...)
+	return m
+}
