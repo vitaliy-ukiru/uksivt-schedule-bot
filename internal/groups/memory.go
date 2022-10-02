e3e2c8ebe2b7b43ce766f86d377b7116cc19f153
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-10-02 23:37:58 +0500

    rename groups.FileSystem to InMemory, delete bad code

---
+++ internal/groups/memory.go
@@ -0,0 +1,67 @@
+package groups
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"sort"
+)
+
+type InMemoryService struct {
+	//        year       spec     num
+	groups map[int]map[string][]int
+}
+
+func (i InMemoryService) Years() []int {
+	years := make([]int, 0, len(i.groups))
+	for year := range i.groups {
+		years = append(years, year)
+	}
+	sort.Ints(years)
+	return years
+}
+
+func (i InMemoryService) Specs(year int) []string {
+	v, ok := i.groups[year]
+	if !ok {
+		return nil
+	}
+	specs := make([]string, 0, len(v))
+	for spec := range v {
+		specs = append(specs, spec)
+	}
+	sort.Strings(specs)
+	return specs
+}
+
+func (i InMemoryService) Numbers(year int, spec string) []int {
+	specs, ok := i.groups[year]
+	if !ok {
+		return nil
+	}
+	numbers := append([]int(nil), specs[spec]...)
+	sort.Ints(numbers)
+	return numbers
+}
+
+func NewInMemoryService(groups map[int]map[string][]int) *InMemoryService {
+	return &InMemoryService{groups: groups}
+}
+
+func NewInMemoryFromReader(reader io.Reader) (*InMemoryService, error) {
+	m := make(map[int]map[string][]int)
+	if err := json.NewDecoder(reader).Decode(&m); err != nil {
+		return nil, err
+	}
+
+	return &InMemoryService{groups: m}, nil
+}
+
+func NewInMemoryFromFile(path string) (*InMemoryService, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return NewInMemoryFromReader(file)
+}
