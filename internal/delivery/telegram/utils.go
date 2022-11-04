ae0027e27b3e598f6c8290a73d75343dbd29f2d1
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-11-04 23:23:51 +0500

    delete "getStartOfWeek" from utils.go

--- internal/delivery/telegram/utils.go
+++ internal/delivery/telegram/utils.go
@@ -2,7 +2,6 @@ package telegram
 
 import (
 	"context"
-	"time"
 
 	"github.com/vitaliy-ukiru/uksivt-schedule-bot/internal/domain/chat"
 	"go.uber.org/zap"
@@ -20,14 +19,4 @@ func (h Handler) getChat(tgID int64) *Chat {
 		return nil
 	}
 	return &Chat{Chat: chatObj, Status: status}
-}
-
-func getStartDayOfWeek(tm time.Time) time.Time { //get monday 00:00:00
-	weekday := time.Duration(tm.Weekday())
-	if weekday == 0 {
-		weekday = 7
-	}
-	year, month, day := tm.Date()
-	currentZeroDay := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-	return currentZeroDay.Add(-1 * (weekday - 1) * 24 * time.Hour)
 }
