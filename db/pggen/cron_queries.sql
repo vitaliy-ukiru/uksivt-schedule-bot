eb6dd66412461175794852b0bcb48f28a23bd771
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-11-23 23:28:48 +0500

    create cron queries

---
+++ db/pggen/cron_queries.sql
@@ -0,0 +1,42 @@
+--name: CreateJob :one
+INSERT INTO scheduler_jobs(chat_id, send_at, flags)
+VALUES (pggen.arg('ChatID'),
+        pggen.arg('SendAt'),
+        pggen.arg('Flags'))
+RETURNING id;
+
+
+--name: FindByID :one
+SELECT id, chat_id, send_at, flags
+FROM scheduler_jobs
+WHERE id = pggen.arg('ID');
+
+--name: FindAtTime :many
+SELECT id, chat_id, send_at, flags
+FROM scheduler_jobs
+WHERE send_at >= pggen.arg('At')::timetz - pggen.arg('Period')::interval
+  AND send_at <= pggen.arg('At')::timetz
+ORDER BY id, send_at;
+
+--name: FindByChat :many
+SELECT id, chat_id, send_at, flags
+FROM scheduler_jobs
+WHERE chat_id = pggen.arg('ChatID')
+ORDER BY id;
+
+
+--name: UpdateTime :exec
+UPDATE scheduler_jobs
+SET send_at = pggen.arg('SendAt')
+WHERE id = pggen.arg('ID');
+
+--name: UpdateFlags :exec
+UPDATE scheduler_jobs
+SET flags = pggen.arg('Flags')
+WHERE id = pggen.arg('ID');
+
+
+--name: Delete :exec
+DELETE
+FROM scheduler_jobs
+WHERE id = pggen.arg('ID');
