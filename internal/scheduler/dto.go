5213098921a09c7b37b4674981bc09a8bc59b091
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-11-30 02:06:06 +0500

    scheduler: dto init

---
+++ internal/scheduler/dto.go
@@ -0,0 +1,14 @@
+package scheduler
+
+import "time"
+
+type CreateJobDTO struct {
+	At     time.Time `json:"at"`
+	Flags  uint16    `json:"flags,omitempty"`
+	ChatID int64     `json:"chat_id,omitempty"`
+}
+
+type UpdateDTO struct {
+	NewTime  *time.Time `json:"new_time,omitempty"`
+	NewFlags *uint16    `json:"new_flags,omitempty"`
+}
