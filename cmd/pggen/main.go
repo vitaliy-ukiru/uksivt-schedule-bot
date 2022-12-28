bc8494723f5ab06aeb54021cd16f204c9592f050
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-12-28 20:23:19 +0500

    pggen: new alias `smalling => uint16`

--- cmd/pggen/main.go
+++ cmd/pggen/main.go
@@ -29,6 +29,7 @@ func main() {
 		TypeOverrides: map[string]string{
 			"integer":  "int64",
 			"bigint":   "int64",
+			"smallint": "uint16",
 			"text":     "github.com/jackc/pgtype.Text",
 			"time":     "time.Time",
 			"interval": "time.Duration",
