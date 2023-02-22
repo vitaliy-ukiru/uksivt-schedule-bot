581c27ec3332725ec6234aae1a820fb444df71a4
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-02-23 03:44:08 +0500

    add skipping crons with ForNextDay flag at Saturday (on Sunday)

--- internal/delivery/telegram/controller/schedule/cron_scheduler.go
+++ internal/delivery/telegram/controller/schedule/cron_scheduler.go
@@ -21,27 +21,27 @@ func (h *Handler) CronSchedulerJob() {
 
 	crons, err := h.crons.At(ctx, now)
 	if err != nil {
-		h.logger.With(zap.Error(err)).Error("cannot fetch crons")
+		h.logger.Error("cannot fetch crons", zap.Error(err))
 	}
 
 	h.logger.Debug("crons fetched", zap.Int("count", len(crons)))
 	for _, cron := range crons {
 		logger := h.logger.With(zap.Int64("cron_id", cron.ID))
-		logger.Debug("starting cron", zap.Int64("cron_id", cron.ID))
+		logger.Debug("starting cron")
+		day := now
+		if cron.Flags.Has(scheduler.NextDay) {
+			if day.Weekday() == time.Saturday {
+				// skipping because Sunday is not study day
+				logger.Debug("skip cron, reason:ForNextDay+Saturday")
+				continue
+			}
+
+			day = day.Add(24 * time.Hour)
+		}
+
 		c, err := h.chats.ByID(ctx, cron.ChatID)
 		if err != nil {
 			logger.Error("cannot get chat", zap.Error(err))
-		}
-
-		day := now
-		if cron.Flags.Has(scheduler.NextDay) {
-			forNextDay := 24 * time.Hour
-
-			if day.Weekday() == time.Saturday {
-				forNextDay *= 2
-			}
-
-			day = day.Add(forNextDay)
 		}
 
 		lessons, err := h.uksivt.LessonsOneDay(ctx, *c.Group, day)
@@ -134,14 +134,14 @@ func (h *Handler) cronReplaces(p *cronParams) error {
 	}
 
 	if len(replaces) == 0 {
-		if p.Cron.Flags.Has(scheduler.ReplacesAlways) {
-			_, err := h.bot.Send(
-				tele.ChatID(p.Chat.TgID),
-				fmt.Sprintf("Замен на %s не найдено", p.Day.Format("02.01.2006")),
-			)
-			return err
+		if !p.Cron.Flags.Has(scheduler.ReplacesAlways) {
+			return nil
 		}
-		return nil
+		_, err := h.bot.Send(
+			tele.ChatID(p.Chat.TgID),
+			fmt.Sprintf("[CRON:%s] Замен на %s не найдено", p.Cron.Title, p.Day.Format("02.01.2006")),
+		)
+		return err
 	}
 
 	_, err := h.bot.Send(
