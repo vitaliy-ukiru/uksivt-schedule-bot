c230503b107e6b2bc0b4be25600bf139966e2600
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-02-28 12:43:20 +0500

    add translation mode

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 -- TODO PROPOSAL[GROUP_TYPE]: create table groups (id, year, spec, num)
 -- TODO PROPOSAL[GROUP_TYPE]: change column chats.college_group to coll_group_id (FK to groups.id)
 -- TODO PROPOSAL[CHAT_PK]: delete column ID, use telegram id as PRIMARY KEY
-
+BEGIN;
 create table if not exists chats
 (
     id            bigint generated always as identity
@@ -18,8 +18,9 @@ create table if not exists crons
     id      bigint generated always as identity
         primary key,
     chat_id integer      not null
-            references chats,
-    title varchar(200) not null,
-    send_at time     not null,
-    flags   smallint not null
+        references chats,
+    title   varchar(200) not null,
+    send_at time         not null,
+    flags   smallint     not null
 );
+COMMIT;
