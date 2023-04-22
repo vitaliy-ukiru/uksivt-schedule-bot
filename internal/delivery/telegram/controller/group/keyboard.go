631e2c0e60fbd6691a93bd6dc781d6370059c1a5
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-04-23 01:19:11 +0500

    add back button to menus, using builder with buffers

--- internal/delivery/telegram/controller/group/keyboard.go
+++ internal/delivery/telegram/controller/group/keyboard.go
@@ -10,13 +10,15 @@ import (
 const SGCallback = "select_g"
 
 var (
-	CancelBtn = keyboard.CallbackButton("Отмена", SGCallback+"_cancel")
 	AcceptBtn = keyboard.CallbackButton("Подтвердить", SGCallback+"_accept")
-	//BackBtn   = keyboard.CallbackButton("Назад", "select.g.back")
+	CancelBtn = keyboard.CallbackButton("Отмена", SGCallback+"_cancel")
+	BackBtn   = keyboard.CallbackButton("Назад", SGCallback+"_back")
+
+	CancelRow = tele.Row{BackBtn, CancelBtn}
 )
 
 func SelectYearMarkup(years []int) *tele.ReplyMarkup {
-	b := keyboard.NewBuilder(1)
+	b := keyboard.NewBuilderBuffer(1, len(years)+1)
 	for _, year := range years {
 		yearStr := strconv.Itoa(year)
 		btn := keyboard.CallbackButton(yearStr+" год", SGCallback, yearStr)
@@ -29,33 +31,31 @@ func SelectYearMarkup(years []int) *tele.ReplyMarkup {
 func SelectSpecMarkup(year int, specs []string) *tele.ReplyMarkup {
 	yearStr := strconv.Itoa(year)
 	const rowSize = 3.0
-	b := keyboard.NewBuilder(rowSize)
+	b := keyboard.NewBuilderBuffer(rowSize, len(specs)/rowSize+1)
 	for _, spec := range specs {
 		b.Insert(keyboard.CallbackButton(yearStr+spec, SGCallback, spec))
 	}
-	b.OneButtonRow(CancelBtn)
+	b.Row(CancelRow)
 
 	return b.Inline()
 }
 
 func SelectNumberMarkup(year int, spec string, numbers []int) *tele.ReplyMarkup {
 	groupStr := strconv.Itoa(year) + spec + "-"
-	b := keyboard.NewBuilder(1)
+	b := keyboard.NewBuilderBuffer(1, len(numbers)+1)
 	for _, number := range numbers {
 		numberStr := strconv.Itoa(number)
 		btn := keyboard.CallbackButton(groupStr+numberStr, SGCallback, numberStr)
 		b.OneButtonRow(btn)
 	}
-	b.OneButtonRow(CancelBtn)
+	b.Row(CancelRow)
 	return b.Inline()
 }
 
 func AcceptMarkup() *tele.ReplyMarkup {
-	b := keyboard.NewBuilder(1)
-	b.Add(
-		AcceptBtn,
-		CancelBtn,
-	)
+	b := keyboard.NewBuilderBuffer(1, 2)
+	b.OneButtonRow(AcceptBtn)
+	b.Row(CancelRow)
 	return b.Inline()
 
 }
