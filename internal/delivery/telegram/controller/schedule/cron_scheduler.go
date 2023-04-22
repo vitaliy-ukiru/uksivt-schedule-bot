8780ad08bacd1ef3ccaf3e4651e83670122b70c9
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-04-23 02:22:24 +0500

    edit log text of starting

--- internal/delivery/telegram/controller/schedule/cron_scheduler.go
+++ internal/delivery/telegram/controller/schedule/cron_scheduler.go
@@ -14,7 +14,7 @@ import (
 
 func (h *Handler) CronSchedulerJob() {
 	now := time.Now()
-	h.logger.Info("start cron job")
+	h.logger.Info("starting processing scheduled cron")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
